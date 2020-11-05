2a5257abe1393ea7db3465cc4754126d940ce9bf
Ed Ackerman <edack@kengel.com>
2020-11-05 18:36:22 -0500

    .

--- covid19/scripts/parseus.ts
+++ covid19/scripts/parseus.ts
@@ -4,30 +4,32 @@ import * as fs from "fs";
 const https = require('https');
 let data = '';
 
-https.get('https://api.covid19api.com/summary', (res: any) => {
+https.get('https://api.covidtracking.com/v1/us/daily.json', (res: any) => {
 
   res.on('data', (chunk: any) => {
     data += chunk;
-    // console.log('data', data);
+//    console.log('data', data);
   });
 
   res.on('end', () => {
     const getCovidData = JSON.parse(data);
-    // console.log(getCovidData);
-    var countries = getCovidData.Countries;
-    var fields = Object.keys(countries[0]);
-    console.log(countries);
+    var row = getCovidData.row;
+//    console.log(getCovidData);
+    var fields = Object.keys(getCovidData);
+//    console.log(fields)
+
+    console.log(getCovidData.row)
     var replacer = function(key: any, value: any) { return value === null ? '' : value };
-    var csv = countries.map(function(row: { [x: string]: any; }){
-      return fields.map(function(fieldName){
-        return JSON.stringify(row[fieldName], replacer)
+    var csv = getCovidData.map(function(row: { [x: string]: any; }){
+       return fields.map(function(fieldName){
+         return JSON.stringify(row[fieldName], replacer)
       }).join(',')
     });
     csv.unshift(fields.join(','));
     const convertedCSV = csv.join('\r\n');
     // console.log(csv)
 
-    fs.writeFileSync("./build/countries.txt", convertedCSV);
+    fs.writeFileSync("./build/usahist.txt", convertedCSV);
 
   });
 
