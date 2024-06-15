50789a840ea863f055b0e3f94e753ed8fcd12498
Ed Ackerman <edack@kengel.com>
2024-06-15 18:40:04 -0400

    new file:   xfer.bat

---
+++ xfer.bat
@@ -0,0 +1,3 @@
+zowe zos-files ul dir-to-pds "cbl" "z00737.cbl" --zosmf-p  zXplorer
+zowe zos-files ul dir-to-pds "jcl" "z00737.jcl" --zosmf-p  zXplorer
+zowe zos-files ul dir-to-pds "copylib" "z00737.copylib" --zosmf-p  zXplorer
